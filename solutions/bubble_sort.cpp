ee224d09d4d4530bcf69daac278fb7dda79f7ab8
xiyao <futuretech6@foxmail.com>
2023-09-16 01:04:45 +0800

    feat: separate swap

--- solutions/bubble_sort.cpp
+++ solutions/bubble_sort.cpp
@@ -1,12 +1,17 @@
 #include <solution.h>
 
+template <typename T>
+void swap(T &a, T &b) {
+    T tmp = a;
+    a     = b;
+    b     = tmp;
+}
+
 void Solution::sort(std::vector<int> &array) {
     for (auto i = array.begin(); i != array.end(); i++) {
         for (auto j = i + 1; j != array.end(); j++) {
             if (*i > *j) {
-                auto tmp = *i;
-                *i       = *j;
-                *j       = tmp;
+                swap(*i, *j);
             }
         }
     }
